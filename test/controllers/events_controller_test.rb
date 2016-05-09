3f15089bd50815f03fd5ba6822a32f0125405892
boomingbrake <brittanywebbe@gmail.com>
2016-05-09 12:17:17 -0400

    Move test into generated test files in test directories

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,9 @@
-require 'test_helper'
+require_relative '../test_helper'
 
 class EventsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:events)
+  end
 end
