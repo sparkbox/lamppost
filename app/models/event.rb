caa8fa1ef1aad2d67819c709504c257c97d95419
BC3209 <bchidoro9@gmail.com>
2016-05-11 09:58:47 -0400

    Add additional tests for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ require_relative "../../lib/blacklist"
 class Event < ActiveRecord::Base
   before_create :validate_name
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 7, maximum: 100}
   validates_uniqueness_of :name
 end
 
