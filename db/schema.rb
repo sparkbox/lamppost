2a49d1f9adabfa314981ba4a68d50a5c7bf7fb5d
boomingbrake <brittanywebbe@gmail.com>
2016-05-17 14:02:25 -0400

    Add Logo Image Uploader to Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
+ActiveRecord::Schema.define(version: 20160517152139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
