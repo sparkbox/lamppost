34d075d729e8f7416abed62046e1a9c0f7395596
BC3209 <bchidoro9@gmail.com>
2016-05-20 12:26:23 -0400

    Add location field back

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520150354) do
+ActiveRecord::Schema.define(version: 20160520160701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160520150354) do
     t.text     "city"
     t.text     "state"
     t.text     "zipcode"
+    t.text     "location"
   end
 
   create_table "links", force: :cascade do |t|
