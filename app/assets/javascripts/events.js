f71886a16e8caed298f1cb64a4d11cd1e1885c1b
iAmNathanJ <jacobs@heysparkbox.com>
2016-05-23 21:58:24 -0400

    JS: Update events js file
    - add more commenting
    - reveal filters via js
    - wrap everything in main function
    - run script on DOMContentLoaded event

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,66 +1,89 @@
-// DOM Selections
-var DOM = {
-  body: select.element('body'),
-  sideBar: select.element('.side-bar'),
-  siteHeader: select.element('.site-header'),
-  siteLogo: select.element('.site-logo'),
-  btnToggleFilters: select.element('.event-filters__toggle-button'),
-  eventFilters: select.element('.event-filters__filters'),
-  shareableLinks: select.all('.shareable-link'),
-  scrollPosition: function() {
-    return document.documentElement.scrollTop || DOM.body.scrollTop;
+function lampPostMain() {
+
+  /*
+  ================================================================
+    DOM Selections & State
+  ================================================================
+  */
+  var DOM = {
+    body: select.element('body'),
+    sideBar: select.element('.side-bar'),
+    siteHeader: select.element('.site-header'),
+    siteLogo: select.element('.site-logo'),
+    eventFilters: select.element('.event-filters'),
+    eventFiltersToggleButton: select.element('.event-filters__toggle-button'),
+    eventFiltersFilterGroups: select.element('.event-filters__filters'),
+    shareableLinks: select.all('.shareable-link'),
+    scrollPosition: function() {
+      return document.documentElement.scrollTop || DOM.body.scrollTop;
+    }
+  };
+
+
+  /*
+  ================================================================
+    Page Init
+  ================================================================
+  */
+  // Reveal the filters. They are display:none unless this script loads
+  DOM.eventFilters.style.display = 'block';
+
+
+  /*
+  ================================================================
+    Event Listeners
+  ================================================================
+  */
+  window.addEventListener('scroll', throttle(updateHeader, 80));
+  DOM.eventFiltersToggleButton.addEventListener('click', toggleFilters);
+  DOM.shareableLinks.forEach(setupShareableLink);
+
+
+  /*
+  ================================================================
+  Event Handlers
+  ================================================================
+  */
+  function shrinkHeader() {
+    DOM.siteHeader.classList.add('compact');
   }
-};
 
-
-// Page Init
-DOM.sideBar.classList.add('hide-filters'); // Hide filters on mobile view by default
-
-
-// Event Listeners
-window.addEventListener('scroll', throttle(updateHeader, 80));
-DOM.btnToggleFilters.addEventListener('click', toggleFilters);
-DOM.shareableLinks.forEach(setupShareableLink);
-
-
-// Event Handlers
-function shrinkHeader() {
-  DOM.siteHeader.classList.add('compact');
-}
-
-function expandHeader() {
-  DOM.siteHeader.classList.remove('compact');
-}
-
-function updateHeader(e) {
-  if(DOM.scrollPosition() > 40) {
-    shrinkHeader();
-  } else {
-    expandHeader();
+  function expandHeader() {
+    DOM.siteHeader.classList.remove('compact');
   }
-}
 
-function toggleFilters() {
-  DOM.sideBar.classList.toggle('hide-filters');
-}
+  function updateHeader(e) {
+    if(DOM.scrollPosition() > 40) {
+      shrinkHeader();
+    } else {
+      expandHeader();
+    }
+  }
 
-function setupShareableLink(linkComponent) {
-  var linkInput = select.child('.shareable-url-text-input').from(linkComponent);
+  function toggleFilters() {
+    DOM.sideBar.classList.toggle('filters-open');
+  }
 
-  linkInput.addEventListener('click', function(e) {
-    linkInput.select();
-  });
+  function setupShareableLink(linkComponent) {
+    var linkInput = select.child('.shareable-url-text-input').from(linkComponent);
 
-  if(document.queryCommandSupported('copy')) {
-    copyButton = document.createElement('button');
-    copyButton.classList.add('card-button', 'copy-button');
-    copyButton.setAttribute('title', 'Copy Link');
-    copyButton.innerHTML = 'Copy Link to Clipboard';
-    linkComponent.appendChild(copyButton);
-
-    copyButton.addEventListener('click', function(e) {
+    linkInput.addEventListener('click', function(e) {
       linkInput.select();
-      document.execCommand('copy');
     });
+
+    if(document.queryCommandSupported('copy')) {
+      copyButton = document.createElement('button');
+      copyButton.classList.add('card-button', 'copy-button');
+      copyButton.setAttribute('title', 'Copy Link');
+      copyButton.innerHTML = 'Copy Link to Clipboard';
+      linkComponent.appendChild(copyButton);
+
+      copyButton.addEventListener('click', function(e) {
+        linkInput.select();
+        document.execCommand('copy');
+      });
+    }
   }
 }
+
+document.addEventListener('DOMContentLoaded', lampPostMain);
