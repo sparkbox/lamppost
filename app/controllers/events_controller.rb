f5e71f93f60ee256075421c46b2256ed0a725ba5
boomingbrake <brittanywebbe@gmail.com>
2016-05-25 13:25:45 -0400

    Add show view for event detail view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,24 @@ class EventsController < ApplicationController
     end
   end
 
+  def show
+    @event = Event.find(params[:id])
+
+    respond_to do |format|
+      format.html  # show.html.erb
+      format.json  { render :json => @event }
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event).permit(:name, :id)
+    end
 end
