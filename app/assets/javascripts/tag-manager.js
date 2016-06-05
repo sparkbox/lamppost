a7bca4475db6b8b17d397c4d68f6f3edb1f9f52c
iAmNathanJ <jacobs@heysparkbox.com>
2016-06-05 15:28:17 -0400

    Update tag-manager.js, now supports tag categories

--- app/assets/javascripts/tag-manager.js
+++ app/assets/javascripts/tag-manager.js
@@ -1,34 +1,69 @@
-function TagManager() {
-  var activeTags = [];
+function TagManager(initTags) {
 
-  function add(tag) {
-    if(!have(tag)) {
-      return activeTags.push(tag);
-    }
+  var _activeTags = initTags || {};
+
+  function _exists(item, arr, fromIndex) {
+    return arr.indexOf(item, (fromIndex || 0)) > -1;
   }
 
-  function remove(tagToRemove) {
-    if(have(tagToRemove)) {
-      return activeTags = activeTags.filter(function(tag) {
-        return tag !== tagToRemove;
+  function add(tagList, category) {
+    if(!_activeTags.hasOwnProperty(category)) {
+      _activeTags[category] = tagList || [];
+    } else {
+      var newTags = tagList.filter(function(tag) {
+        return !_exists(tag, _activeTags[category]);
       });
+      Array.prototype.push.apply(_activeTags[category], newTags);
     }
+    return _activeTags;
   }
 
-  function have(tagToTest) {
-    return activeTags.filter(function(tag) {
-      return tag === tagToTest;
+  function remove(removalList, category) {
+    if(!_activeTags.hasOwnProperty(category)) return;
+    _activeTags[category] = _activeTags[category].filter(function(tag) {
+      return !_exists(tag, removalList);
+    });
+    return _activeTags;
+  }
+
+  function has(tag, category) {
+    var categories = category ? [category] : Object.getOwnPropertyNames(_activeTags);
+    return categories.filter(function(cat) {
+      return _exists(tag, _activeTags[cat]);
     }).length > 0;
   }
 
-  function list() {
-    return activeTags;
+  function category(cat) {
+    return {
+      list: function() {
+        return _activeTags[cat];
+      },
+      has: function(tag) {
+        return has(tag, cat);
+      }
+    };
+  }
+
+  function tags() {
+    return _activeTags;
+  }
+
+  function serializeTags() {
+    var categoryNames = Object.getOwnPropertyNames(_activeTags),
+        categoryDelimiter;
+    var queryString = categoryNames.reduce(function(qs, category, i) {
+      categoryDelimiter = i < (categoryNames.length - 1) ? '&' : '';
+      return qs + category  + '=' + _activeTags[category].join(',') + categoryDelimiter;
+    }, '?');
+    return encodeURI(queryString);
   }
 
   return {
     add: add,
     remove: remove,
-    have: have,
-    list: list
+    has: has,
+    category: category,
+    tags: tags,
+    serializeTags: serializeTags
   };
 }
