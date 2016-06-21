6aa8ab87c22970bdd6c2aebcdfa6b1325f4f2457
boomingbrake <brittanywebbe@gmail.com>
2016-06-21 10:27:09 -0400

    Remove filter/tag casing logic. Show as entered

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module LampPost
     config.autoload_paths += %W( #{config.root}/lib/util )
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActsAsTaggableOn.strict_case_match = true
   end
 end
