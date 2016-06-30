a5ba7c56bcc9a58aab88ad1adea6e573d23580da
Nathan Jacobs <jacobs.123@gmail.com>
2016-06-30 14:43:50 -0400

    Feat: Load font only on lamppost.io

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160622160617) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",         null: false
@@ -42,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160622160617) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "links", ["event_id"], name: "index_links_on_event_id", using: :btree
+  add_index "links", ["event_id"], name: "index_links_on_event_id"
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
@@ -54,15 +51,14 @@ ActiveRecord::Schema.define(version: 20160622160617) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
   end
 
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
-  add_foreign_key "links", "events"
 end
