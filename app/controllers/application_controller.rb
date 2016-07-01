b5818edf785d83e4c57fe13efad31ff275f4668d
Nathan Jacobs <jacobs.123@gmail.com>
2016-07-01 10:14:22 -0400

    Feat: Rredirect www to https://

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :redirect_subdomain
+  
+  def redirect_subdomain
+    if request.host == "www.#{request.domain}.io"
+      redirect_to "https://#{request.domain}.io" + request.fullpath
+    end
+  end
+
 end
